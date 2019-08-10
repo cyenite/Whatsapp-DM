cb94c761bce066a5f10e53dbeeba93bf09864b6c
Akron254 <aaronrono42@gmail.com>
2019-08-10 13:06:39 +0300

    JUnit4 error fixed

--- app/src/androidTest/java/com/kenova/whatsapp/dm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kenova/whatsapp/dm/ExampleInstrumentedTest.java
@@ -5,8 +5,9 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.*;
 
+
 import org.junit.Test;
- import org.junit.runner.RunWith;b
+import org.junit.runner.RunWith;
 
 
 import static org.junit.Assert.*;
